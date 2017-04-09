68451a836d4c06ed105b13fcd2487595a6ef762d
IvanFadieiev <ivanfadeev91@gmail.com>
2017-04-09 19:26:21 +0300

    SessionAuth refactoring

--- app/controllers/concerns/session_auth.rb
+++ app/controllers/concerns/session_auth.rb
@@ -1,37 +1,57 @@
 module SessionAuth
   extend ActiveSupport::Concern
 
-  StepsCount     = 10
-  ExpirationTime = 20
+  STEPS_COUNT     = 10
+  EXPIRATION_TIME = 200.seconds
 
   included do
-    after_action :check_steps_count, :check_session_time
+    after_action :step_relogin!, :time_relogin!
   end
 
-  private
+  protected
 
-  def check_steps_count
+  def step_relogin!
+    steps_count_incrementing
+    change_session_user_token! && session.delete(:steps_count) if check_steps_count
+  end
+
+  def time_relogin!
+    if current_user && check_time
+      current_user.touch(:expire_session)
+    end
+  end
+
+  def regenerate_token!
+    current_user.generate_token && current_user.save
+  end
+
+  def check_time
+    current_user.expire_session < Time.now - expiration_time
+  end
+
+  def steps_count
+    STEPS_COUNT
+  end
+
+  def expiration_time
+    EXPIRATION_TIME
+  end
+
+  def steps_count_incrementing
     session[:steps_count] ||= 0
     session[:steps_count]  += 1
-    regenerate_token && session.delete(:steps_count) if session[:steps_count] >= StepsCount
   end
 
-  def regenerate_token
+  def change_session_user_token!
     if current_user
-      current_user.generate_token && current_user.save
+      regenerate_token!
       new_token = current_user.token
       session.delete(:token)
       session[:token] = new_token
     end
   end
 
-  def check_session_time
-    if current_user && check_time
-      current_user.touch(:expire_session)
-    end
-  end
-
-  def check_time
-    current_user.expire_session < Time.now - ExpirationTime.seconds
+  def check_steps_count
+    session[:steps_count] >= steps_count
   end
 end
